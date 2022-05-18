d262ff2a9bbf8c9420a2d5ec4e72057ef1348f46
Talique <ropars.k@gmail.com>
2022-05-18 14:05:43 +0200

    initial commit

---
+++ ctrl/ctrl_reglages.php
@@ -0,0 +1,4 @@
+<?php
+    include "./view/view_header.html";
+
+?>
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+    include "./view/view_header.html";
+    include "./view/view_accueil.html";
+
+    //session start
+    session_start();
+    //Analyse de l'URL avec parse_url() et retourne ses composants
+    $url = parse_url($_SERVER['REQUEST_URI']);
+    //test soit l'url a une route sinon on renvoi à la racine
+    $path = isset($url['path']) ? $url['path'] : '/';
+
+
+    if(isset($_POST['valider'])){
+        // echo("powet");
+        header('Location: ./reglages');
+    }
+
+    var_dump($path);
+    /*------------------------------------------------
+                        ROUTEUR
+    ------------------------------------------------*/
+    //test de la valeur $path dans l'URL et import de la ressource
+    switch($path){
+        
+        case $path === '/reglages' : 
+            // include './ctrl/ctrl_reglages.php';
+                break ;
+    }
+
+?>
