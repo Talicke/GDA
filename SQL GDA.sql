61d19e4ea27616b8f2c9cb7c69751e5d9f64cab5
Talique <ropars.k@gmail.com>
2022-07-11 14:49:59 +0200

    none

---
+++ SQL GDA.sql
@@ -0,0 +1,138 @@
+CREATE database GDA CHARACTER SET utf8 COLLATE utf8_general_ci;
+use GDA;
+
+create table comptes(
+id_compte int auto_increment primary key not null,
+login_compte varchar(50) not null,
+password_compte varchar(50) not null,
+auth_compte varchar(50) not null,
+estValide bool not null
+)engine=InnoDB;
+
+create table projets(
+id_projet int auto_increment primary key not null,
+nom_projet varchar(255) not null,
+id_compte int not null,
+id_activite int null
+)engine=InnoDB;
+
+create table activites(
+id_activite int auto_increment primary key not null,
+nom_activite varchar(255) not null,
+temps_activite float not null,
+id_compte int not null,
+id_freq int not null
+)engine=InnoDB;
+
+create table frequences(
+id_freq int auto_increment primary key not null,
+intituler_freq varchar(50) not null
+)engine=InnoDB;
+
+create table notes (
+id_note int auto_increment primary key not null,
+contenu_note text not null,
+date_note datetime not null,
+estTerminer bool not null,
+id_cat int not null,
+id_activite int null,
+id_projet int null,
+id_compte int not null,
+id_rdv int null, 
+id_rappel int null
+)engine=InnoDB;
+
+create table categories(
+id_cat int auto_increment primary key not null,
+intituler_cat varchar(50) not null
+)engine=InnoDB;
+
+create table prester(
+id_note int not null,
+id_tache int not null,
+primary key (id_note, id_tache)
+)engine=InnoDB;
+
+create table taches(
+id_tache int auto_increment primary key not null,
+date_tache datetime not null,
+duree_tache float not null
+)engine=InnoDB;
+
+create table rappels(
+id_rappel int auto_increment primary key not null,
+date_rappel date not null,
+heure_rappel time not null
+)engine=InnoDB;
+
+create table rdv(
+id_rdv int auto_increment primary key not null,
+date_rdv date not null,
+heure_rdv time not null,
+lieu_rdv varchar(255)
+)engine=InnoDB;
+
+alter table projets
+add constraint fk_avoir_compte
+foreign key (id_compte)
+references comptes(id_compte);
+
+alter table projets
+add constraint fk_ourdir_activite
+foreign key(id_activite)
+references activites(id_activite);
+
+alter table activites
+add constraint fk_profiter_compte
+foreign key (id_compte)
+references comptes(id_compte);
+
+alter table activites
+add constraint fk_repeter_frequence
+foreign key(id_freq)
+references frequences (id_freq);
+
+alter table notes
+add constraint fk_assujetir_categorie
+foreign key(id_cat)
+references categories(id_cat);
+
+alter table notes
+add constraint fk_appartenir_activite
+foreign key(id_activite)
+references activites(id_activite);
+
+alter table notes
+add constraint fk_seoir_projet
+foreign key(id_projet)
+references projets(id_projet);
+
+alter table notes
+add constraint fk_ecrivailler_compte
+foreign key(id_compte)
+references comptes(id_compte);
+
+alter table notes
+add constraint fk_etre_rdv
+foreign key (id_rdv)
+references rdv(id_rdv);
+
+alter table notes
+add constraint fk_rappeller_rappel
+foreign key (id_rappel)
+references rappels(id_rappel);
+
+alter table prester
+add constraint fk_prester_note
+foreign key (id_note)
+references notes(id_note);
+
+alter table prester
+add constraint fk_prester_tache
+foreign key (id_tache)
+references taches (id_tache);
+
+INSERT INTO `gda`.`categories` (`intituler_cat`) VALUES ('note');
+INSERT INTO `gda`.`categories` (`intituler_cat`) VALUES ('tache');
+INSERT INTO `gda`.`categories` (`intituler_cat`) VALUES ('rappel');
+INSERT INTO `gda`.`categories` (`intituler_cat`) VALUES ('RDV');
